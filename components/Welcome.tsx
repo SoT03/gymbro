0532ec7804543f0798da68812b35e3dd52834918
SoT03 <bogdan.zygadlo123@gmail.com>
2023-11-24 13:56:02 +0100

    created mobile version of welcome section

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+const videos = [];
 
 const Welcome = () => {
 	return (
@@ -6,33 +6,49 @@ const Welcome = () => {
 			<div className='wrapper'>
 				<div>
 					<div className='p-8 '>
-						<p className='text-orange-300 uppercase text-xs ml-8 -mb-1'>
+						<p className='text-orange-300 uppercase text-xs ml-2 -mb-1'>
 							About gymbro
 						</p>
-						<h2 className='uppercase text-2xl font-bold text-center mb-2'>
+						<h2 className='uppercase text-3xl font-bold text-center mb-2'>
 							Welcome to us
 						</h2>
-						<hr className='bg-orange-400' />
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-							soluta similique cum.
-						</p>
-						<p>
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-							ratione assumenda consectetur deserunt laboriosam minima! Quae sed
-							nostrum vel consequuntur libero atque!
+						<hr className='border-orange-400 border-solid border-2 mb-6' />
+						<h3 className='mb-3'>
+							Welcome to GymBro, where fitness meets community and
+							transformation happens!
+						</h3>
+						<p className='text-sm'>
+							At GymBro, we are more than just a gym, we are a passionate and
+							supportive fitness family dedicated to helping you achieve your
+							health and wellness goals. Our mission is to create a positive and
+							empowering environment that inspires you to challenge yourself,
+							embrace a healthy lifestyle, and discover the best version of
+							yourself.
 						</p>
 					</div>
-					<div>
+					<div className=''>
 						<iframe
+							className='w-full 
+                            '
 							src='https://www.youtube.com/embed/gey73xiS8F4?si=6r2bzxu0FRjBx3Cz'
 							title='YouTube video player'
 							allowFullScreen></iframe>
-						<div>
-							<img src='/static/img/header/togheter_small.jpg' alt='XDD' />
-							<img src='/static/img/header/togheter_small.jpg' alt='XDD' />
-							<img src='/static/img/header/togheter_small.jpg' alt='XDD' />
+						<div className='flex justify-around'>
+							<img
+								src='/static/img/header/togheter_small.jpg'
+								className='w-1/3 border-2'
+								alt='XDD'
+							/>
+							<img
+								src='/static/img/header/togheter_small.jpg'
+								className='w-1/3 border-2'
+								alt='XDD'
+							/>
+							<img
+								src='/static/img/header/togheter_small.jpg'
+								className='w-1/3 border-2'
+								alt='XDD'
+							/>
 						</div>
 					</div>
 				</div>
