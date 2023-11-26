5592d6a933eff61cc9ea438c27227551b6f4939f
SoT03 <bogdan.zygadlo123@gmail.com>
2023-11-26 22:49:39 +0100

    added thumbnails

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,12 +1,26 @@
-const videos = [];
+import { useState } from 'react';
+
+const videos = [
+	'https://www.youtube.com/embed/gey73xiS8F4?si=6r2bzxu0FRjBx3Cz',
+	'https://www.youtube.com/embed/uNN62f55EV0?si=JxekEK8Q30zbwmQV',
+	'https://www.youtube.com/embed/R5nnr_j2uVk?si=RrnefEXYnKgoQBLR',
+];
+
+const thumbnails = [
+	'/static/img/welcome/backguy.jpg',
+	'/static/img/welcome/cbum.jpg',
+	'/static/img/welcome/zyzz.jpg',
+];
 
 const Welcome = () => {
+	const [currentVideo, setCurrentVideo] = useState(0);
+
 	return (
-		<section className='bg-gray-700 text-white overflow-hidden'>
+		<section className='bg-gray-700  text-white overflow-hidden'>
 			<div className='wrapper'>
 				<div>
 					<div className='p-8 '>
-						<p className='text-orange-300 uppercase text-xs ml-2 -mb-1'>
+						<p className='text-orange-300  uppercase text-xs ml-2 -mb-1'>
 							About gymbro
 						</p>
 						<h2 className='uppercase text-3xl font-bold text-center mb-2'>
@@ -28,27 +42,20 @@ const Welcome = () => {
 					</div>
 					<div className=''>
 						<iframe
-							className='w-full 
+							className='w-full  
                             '
-							src='https://www.youtube.com/embed/gey73xiS8F4?si=6r2bzxu0FRjBx3Cz'
+							src={videos[currentVideo]}
 							title='YouTube video player'
 							allowFullScreen></iframe>
 						<div className='flex justify-around'>
-							<img
-								src='/static/img/header/togheter_small.jpg'
-								className='w-1/3 border-2'
-								alt='XDD'
-							/>
-							<img
-								src='/static/img/header/togheter_small.jpg'
-								className='w-1/3 border-2'
-								alt='XDD'
-							/>
-							<img
-								src='/static/img/header/togheter_small.jpg'
-								className='w-1/3 border-2'
-								alt='XDD'
-							/>
+							{thumbnails.map((photo) => (
+								<img
+									src={photo}
+									alt='some alt'
+									key={photo}
+									className='w-1/3 border-2 cursor-pointer hover:scale-110 transition-transform duration-300'
+								/>
+							))}
 						</div>
 					</div>
 				</div>
