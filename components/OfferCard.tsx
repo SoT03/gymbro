da439aa61d18eb1a2caba243e5cc357bd061c886
SoT03 <bogdan.zygadlo123@gmail.com>
2023-12-05 20:56:24 +0100

    created style for mobile devices for offer section

--- components/OfferCard.tsx
+++ components/OfferCard.tsx
@@ -1,17 +1,20 @@
 const OfferCard = () => {
 	return (
-		<div>
-			<div>
-				<p>
-					$49 <span>month</span>
+		<div className='border text-center py-4 my-6'>
+			<div className=''>
+				<p className='mb-2'>
+					<span className='font-bold  text-lg'>
+						$<span className='text-2xl'>49</span>
+					</span>
+					/month
 				</p>
-				<h3>One arm kikcs</h3>
+				<h3 className='bg-zinc-900 py-1 font-bold'>One arm kikcs</h3>
 			</div>
-			<div>
-				<p>Lorem, ipsum dolor.</p>
-				<p>Lorem, ipsum dolor.</p>
-				<p>Lorem, ipsum dolor.</p>
-				<p>Lorem, ipsum dolor.</p>
+			<div className='px-8 py-4 text-slate-300'>
+				<p className='border-b border-slate-200 pb-2'>Lorem, ipsum dolor.</p>
+				<p className='border-b border-slate-200 pb-2'>Lorem, ipsum dolor.</p>
+				<p className='border-b border-slate-200 pb-2'>Lorem, ipsum dolor.</p>
+				<p className='border-b border-slate-200 pb-2'>Lorem, ipsum dolor.</p>
 			</div>
 		</div>
 	);
