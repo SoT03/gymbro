faab2f13878ad31824ff82edc891501e4c226b40
SoT03 <bogdan.zygadlo123@gmail.com>
2023-12-08 14:49:54 +0100

    styled offer card

--- components/Offer.tsx
+++ components/Offer.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image';
+
 const Offer = () => {
 	return (
-		<div>
-			<div></div>
-			<div>
-				<h2>Lorem, ipsum dolor.</h2>
+		<div className='bg-zinc-800  relative flex flex-col'>
+			<div className='relative '>
+				<Image
+					width={640}
+					height={427}
+					src='/static/img/offers/gym.jpg'
+					alt='XD'
+				/>
+			</div>
+			<div className='p-6'>
+				<h2 className='mb-2'>Lorem, ipsum dolor.</h2>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas delectus
 					non sequi ut distinctio cumque maxime placeat facilis minima? Maxime
