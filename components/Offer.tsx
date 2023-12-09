06d51db54c5cc7c7c04a025889e13c2a5341287c
SoT03 <bogdan.zygadlo123@gmail.com>
2023-12-09 16:16:29 +0100

    generating data dynamicly

--- components/Offer.tsx
+++ components/Offer.tsx
@@ -1,23 +1,22 @@
 import Image from 'next/image';
 
-const Offer = () => {
+type OfferProps = {
+	data: {
+		img: string;
+		title: string;
+		desc: string;
+	};
+};
+
+const Offer = ({ data }: OfferProps) => {
 	return (
-		<div className='bg-zinc-800  relative flex flex-col'>
+		<div className='bg-zinc-800  relative flex flex-col my-10 rounded-sm shadow-lg shadow-black'>
 			<div className='relative '>
-				<Image
-					width={640}
-					height={427}
-					src='/static/img/offers/gym.jpg'
-					alt='XD'
-				/>
+				<Image width={640} height={427} src={data.img} alt='XD' />
 			</div>
-			<div className='p-6'>
-				<h2 className='mb-2'>Lorem, ipsum dolor.</h2>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas delectus
-					non sequi ut distinctio cumque maxime placeat facilis minima? Maxime
-					iure ab inventore consectetur velit nesciunt eum blanditiis?
-				</p>
+			<div className='p-6 '>
+				<h2 className='mb-4 text-xl font-semibold'>{data.title}</h2>
+				<p className='text-left'>{data.desc}</p>
 			</div>
 		</div>
 	);
--- components/Offers.tsx
+++ components/Offers.tsx
@@ -4,6 +4,40 @@ import SectionHeading from './util/Section/SectionHeading';
 import HR from './util/Section/SectionHr';
 import SectionTitle from './util/Section/SectionTitle';
 
+const offersList = [
+	{
+		img: '/static/img/offers/gym.jpg',
+		title: 'Gym Zone',
+		desc: 'It is equipped with equipment for all body parts. GymBro has top quality machines from companies such as Technogym, Matrix, Thorn and many other well-known brands. Training in this zone using free weights or freestanding machines will allow you to strengthen the body, build muscle mass, improve strength. The spatial strength zone is friendly for both women and men.',
+	},
+	{
+		img: '/static/img/offers/fitness.jpg',
+		title: 'Fitness Zone',
+		desc: 'Air-conditioned fitness room, adapted for classes of various intensity, where all group classes are held under the supervision of a qualified instructor. The spacious hall is equipped with professional equipment such as steppes, fitness barbells, mats, dumbbells, rubber, balls and trampolines. We have created the best conditions for exercise and relaxation.',
+	},
+	{
+		img: '/static/img/offers/sauna.jpg',
+		title: 'Fitness Zone',
+		desc: 'Air-conditioned fitness room, adapted for classes of various intensity, where all group classes are held under the supervision of a qualified instructor. The spacious hall is equipped with professional equipment such as steppes, fitness barbells, mats, dumbbells, rubber, balls and trampolines. We have created the best conditions for exercise and relaxation.',
+	},
+	{
+		img: '/static/img/offers/relax.jpg',
+		title: 'Fitness Zone',
+		desc: 'Air-conditioned fitness room, adapted for classes of various intensity, where all group classes are held under the supervision of a qualified instructor. The spacious hall is equipped with professional equipment such as steppes, fitness barbells, mats, dumbbells, rubber, balls and trampolines. We have created the best conditions for exercise and relaxation.',
+	},
+	{
+		img: '/static/img/offers/dietician.jpg',
+		title: 'Fitness Zone',
+		desc: 'Air-conditioned fitness room, adapted for classes of various intensity, where all group classes are held under the supervision of a qualified instructor. The spacious hall is equipped with professional equipment such as steppes, fitness barbells, mats, dumbbells, rubber, balls and trampolines. We have created the best conditions for exercise and relaxation.',
+	},
+
+	{
+		img: '/static/img/offers/trainer.jpg',
+		title: 'Fitness Zone',
+		desc: 'Air-conditioned fitness room, adapted for classes of various intensity, where all group classes are held under the supervision of a qualified instructor. The spacious hall is equipped with professional equipment such as steppes, fitness barbells, mats, dumbbells, rubber, balls and trampolines. We have created the best conditions for exercise and relaxation.',
+	},
+];
+
 const Offers = () => {
 	return (
 		<Section classes={['bg-zinc-900']}>
@@ -14,7 +48,9 @@ const Offers = () => {
 					<HR />
 				</div>
 				<div>
-					<Offer />
+					{offersList.map((offer) => (
+						<Offer data={offer} />
+					))}
 				</div>
 			</div>
 		</Section>
