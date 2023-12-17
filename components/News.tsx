532cbfe46ddb3eaf30899c384e720fd05e1ab97c
SoT03 <bogdan.zygadlo123@gmail.com>
2023-12-17 18:49:33 +0100

    added conditionaly data

--- components/News.tsx
+++ components/News.tsx
@@ -1,7 +1,32 @@
 import NewsCard from './NewsCard';
 import Section from './util/Section/Section';
 import SectionHeading from './util/Section/SectionHeading';
+import HR from './util/Section/SectionHr';
 import SectionTitle from './util/Section/SectionTitle';
+
+const newsData = [
+	{
+		title: 'More plates, more dates ?',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam ab incidunt corrupti quo laboriosam magni dolorem quidem, accusantium blanditiis tempora eos voluptatem possimus exercitationem ex vitae cum.',
+		likes: 123,
+		comments: 36,
+		img: '/static/img/news/date.jpg',
+	},
+	{
+		title: 'How to eat properly',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe id, iste delectus, itaque ipsam aliquid deserunt totam at sapiente numquam voluptate. Incidunt porro, facilis adipisci tempora.',
+		likes: 69,
+		comments: 3,
+		img: '/static/img/news/diet.jpg',
+	},
+	{
+		title: 'Best suplements',
+		desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nihil voluptate ullam reiciendis culpa laborum quisquam sit incidunt, inventore molestiae repellendus est in numquam vitae nesciunt?',
+		likes: 222,
+		comments: 67,
+		img: '/static/img/news/suplements.jpg',
+	},
+];
 
 const News = () => {
 	return (
@@ -10,12 +35,13 @@ const News = () => {
 				<div className='text-center'>
 					<SectionHeading text='Articles' />
 					<SectionTitle title='Our Latest News' />
+					<HR />
 				</div>
 
 				<div>
-					<NewsCard />
-					<NewsCard />
-					<NewsCard />
+					{newsData.map((news, index) => (
+						<NewsCard key={news.title} data={news} index={index === 1} />
+					))}
 				</div>
 			</div>
 		</Section>
--- components/NewsCard.tsx
+++ components/NewsCard.tsx
@@ -1,42 +1,46 @@
 import Image from 'next/image';
-import { IconMessage, IconClock, IconThumbUp } from '@tabler/icons-react';
+import { IconMessage, IconThumbUp } from '@tabler/icons-react';
 
-const NewsCard = () => {
+type NewsCardProps = {
+	data: {
+		title: string;
+		desc: string;
+		likes: number;
+		comments: number;
+		img: string;
+	};
+	index: boolean;
+};
+
+const NewsCard = ({ data, index }: NewsCardProps) => {
 	return (
-		<div className='bg-zinc-800 relative grid grid-rows-[3fr,5fr]   my-10 rounded-sm shadow-lg shadow-black sm:grid-rows-none sm:grid-cols-[2fr,3fr] lg:grid-cols-[1fr,2fr] md:my-14'>
-			<div className='relative bg-slate-100 md:h-72 '>
+		<div
+			className={`bg-zinc-900 relative grid grid-rows-[3fr,5fr]   my-10 rounded-sm shadow-lg shadow-black sm:grid-rows-none sm:grid-cols-[2fr,3fr] md:my-14 ${
+				index ? 'lg:grid-cols-[2fr,1fr]' : 'lg:grid-cols-[1fr,2fr]'
+			}`}>
+			<div
+				className={`relative bg-slate-100 md:h-72  ${index ? 'order-1' : ''}`}>
 				<Image
 					className=''
 					fill
 					sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-					src='/static/img/header/improve_small.jpg'
+					src={data.img}
 					alt='XD'
 				/>
 			</div>
 			<div className='p-6 md:px-10 relative'>
-				<h2 className='mb-4 text-xl font-semibold'>
-					Lorem ipsum dolor sit amet.
-				</h2>
-				<p className='text-left mb-4 '>
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate,
-					architecto. Porro tempore sapiente eos? Facilis mollitia natus,
-					molestiae cum qui sapiente perspiciatis odio maiores, soluta id
-					voluptatibus. Quis, rem minima.
-				</p>
-				<div className='flex justify-between w-full md:absolute md:bottom-4 md:w-3/4 lg:bottom-6 lg:w-10/12'>
-					<div className='flex gap-1'>
-						<IconClock /> 2h ago
-					</div>
-					<div className='flex gap-2'>
-						<span className='flex gap-1'>
-							<p>123</p>
-							<IconThumbUp />
-						</span>
-						<span className='flex gap-1'>
-							<p>22</p>
-							<IconMessage />
-						</span>
-					</div>
+				<h2 className='mb-4 text-xl font-semibold'>{data.title}</h2>
+				<p className='text-left mb-4 '>{data.desc}</p>
+				<span className='text-amber-200 '>Read more...</span>
+				<div className='flex justify-end gap-8 w-full md:absolute md:bottom-4 md:w-3/4 lg:bottom-6 lg:w-10/12'>
+					<span className='flex gap-1 md:gap-2'>
+						<p>{data.likes}</p>
+						<IconThumbUp />
+					</span>
+					<span className='flex gap-1 md:gap-2'>
+						<p>{data.comments}</p>
+						<IconMessage />
+					</span>
 				</div>
 			</div>
 		</div>
