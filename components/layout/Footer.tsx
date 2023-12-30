665f2818d5fca2cdb813808282de3b38a4b2aeff
SoT03 <bogdan.zygadlo123@gmail.com>
2023-12-30 18:42:03 +0100

    finished mopbile footer info section

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,51 +1,14 @@
-import Image from 'next/image';
-import {
-	IconBrandFacebookFilled,
-	IconBrandYoutubeFilled,
-	IconBrandTwitterFilled,
-	IconBrandTiktokFilled,
-} from '@tabler/icons-react';
-
-const socialClasses = 'w-6 h-6 m-1';
-
-const socials = [
-	<IconBrandFacebookFilled className={socialClasses} />,
-	<IconBrandYoutubeFilled className={socialClasses} />,
-	<IconBrandTwitterFilled className={socialClasses} />,
-	<IconBrandTiktokFilled className={socialClasses} />,
-];
+import Info from './FooterInfo';
+import Socials from './FooterSocials';
 
 const Footer = () => {
 	const year = new Date().getFullYear();
 
 	return (
 		<footer className='bg-black text-white py-2'>
-			<div className='mb-4'>
-				<div className='flex flex-col items-center '>
-					<h2 className='flex items-center'>
-						<Image
-							height={80}
-							width={80}
-							src='/static/img/logo.png'
-							alt='Gymbro logo'
-							className='w-24 h-24 '
-						/>{' '}
-						<p className=' font-bold text-2xl'>
-							Gym<span className='text-orange-400'>Bro</span>
-						</p>
-					</h2>
-					<p className='font-semibold text-lg'>Social Media</p>
-					<div className='flex mt-2 '>
-						{socials.map((social) => (
-							<div
-								key={Math.random()}
-								className='border-zinc-500 border-[1px] mx-1 p-1 rounded-sm'>
-								{social}
-							</div>
-						))}
-					</div>
-				</div>
-				<div></div>
+			<div className='mb-4 flex flex-col gap-4'>
+				<Socials />
+				<Info />
 			</div>
 			<div className='text-center uppercase'>
 				<p>GymBro {year} &copy; </p>
---
+++ components/layout/FooterInfo.tsx
@@ -0,0 +1,41 @@
+import {
+	IconMapPinFilled,
+	IconMailFilled,
+	IconPhoneFilled,
+} from '@tabler/icons-react';
+
+const iconStyle = 'w-full';
+
+const infoData = [
+	{
+		icon: <IconMapPinFilled />,
+		text: 'Nowy Świat 12, 96-300 Żyrardów',
+	},
+	{
+		icon: <IconMailFilled />,
+		text: 'GymBro@gymBro.com',
+	},
+	{
+		icon: <IconPhoneFilled />,
+		text: '829 942 022',
+	},
+];
+
+const Info = () => {
+	return (
+		<div className='flex flex-col items-center text-center '>
+			<h2 className='text-lg font-semibold '>Quick Info</h2>
+			<div className=''>
+				{' '}
+				{infoData.map((info) => (
+					<div className='flex gap-2 my-2'>
+						{' '}
+						{info.icon} {info.text}
+					</div>
+				))}{' '}
+			</div>
+		</div>
+	);
+};
+
+export default Info;
