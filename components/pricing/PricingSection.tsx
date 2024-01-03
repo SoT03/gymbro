f90582a7c96ae1a4e0bff7de11aff400625eaeab
SoT03 <bogdan.zygadlo123@gmail.com>
2024-01-03 16:43:01 +0100

    finished Pricing page

--- components/pricing/PricingSection.tsx
+++ components/pricing/PricingSection.tsx
@@ -1,3 +1,5 @@
+import HR from '../util/Section/SectionHr';
+
 type sectionProps = {
 	children: React.ReactNode;
 	title: string;
@@ -7,10 +9,11 @@ const PricingSection = ({ children, title }: sectionProps) => {
 	return (
 		<section className='px-8 lg:mt-12'>
 			<div className='wrapper md:mt-6'>
-				<h2 className='text-center text-orange-400 uppercase pt-6 text-xl font-semibold md:text-2xl'>
+				<h2 className='text-center mb-2 text-orange-400 uppercase pt-6 text-xl font-semibold md:text-2xl lg:text-3xl'>
 					Gym areas
 				</h2>
-				<div className='text-white md:flex flex-wrap items-center justify-center md:p-4'>
+				<HR />
+				<div className='text-white md:flex flex-wrap  justify-center md:p-4 '>
 					{children}
 				</div>
 			</div>
--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,28 +1,78 @@
 import PricingCard from '@/components/PricingCard';
 import Layout from '@/components/layout/Layout';
-import PricingSection from '@/components/pricing/pricingSection';
+import PricingSection from '@/components/pricing/PricingSection';
 
-let lol = {
-	title: 'Fitness Care',
-	price: { amount: 696, per: 'year' },
-	options: [
-		'24/7 gym entry',
-		'Every gym zone available',
-		'Personal trainer',
-		'Group fitness workout',
-		'Saun avaible',
-		'Own locker',
-	],
-};
+const gymAreasArray = [
+	{
+		title: 'One Day',
+		price: { amount: 8, per: 'Day' },
+		options: ['One day entry', 'Every gym zone available'],
+	},
+	{
+		title: 'Veteran Open Gym',
+		price: { amount: 480, per: 'year' },
+		options: [
+			'24/7 gym entry',
+			'Every gym zone available',
+			'Only 40$ per month',
+			'Saun avaible',
+			'Own locker',
+			'Member t-shirt',
+		],
+	},
+	{
+		title: 'Month Subscription',
+		price: { amount: 50, per: 'Month' },
+		options: ['One day entry', 'Every gym zone available', 'Saun avaible'],
+	},
+];
+
+const bodyBuildingArray = [
+	{
+		title: 'Diet + Plan',
+		price: { amount: 40, per: '3 Months' },
+		options: ['3 Month Plan', 'Workout plan and diet'],
+	},
+	{
+		title: 'Full Care',
+		price: { amount: 210, per: '3 Months' },
+		options: [
+			'3 Month Plan',
+			'Workout plan and diet',
+			'Meals recipies',
+			'Physiotherapist care',
+			'Personal Trainer',
+			'All Gym zones avaible',
+			'Saun avaible',
+		],
+	},
+	{
+		title: 'Fittness Groups',
+		price: { amount: 40, per: 'Month' },
+		options: [
+			'24/7 gym entry',
+			'Every gym zone available',
+			'Personal trainer',
+			'Group fitness workout',
+			'Saun avaible',
+			'Own locker',
+		],
+	},
+];
 
 const PricingPage = () => {
 	return (
 		<Layout>
 			<main className=' bg-zinc-800 py-12 '>
 				<PricingSection title='GYM AREAS'>
-					<PricingCard data={lol} />
-					<PricingCard data={lol} />
-					<PricingCard data={lol} />
+					{gymAreasArray.map((item, index) => (
+						<PricingCard data={item} index={index} key={index} />
+					))}
+				</PricingSection>
+				<PricingSection title='Work with Proffesionalist'>
+					{bodyBuildingArray.map((item, index) => (
+						<PricingCard data={item} index={index} key={index} />
+					))}
 				</PricingSection>
 			</main>
 		</Layout>
